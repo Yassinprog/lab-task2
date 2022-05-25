06a90afe56493d53e22fc6d0eb91496e4129558f
Yassinprog <yassinkarrat10gmail.com>
2022-05-25 11:38:52 +0200

    task3

--- lab2.php
+++ lab2.php
@@ -4,3 +4,11 @@ function first(string $text): void
     echo ($text);
 }
 first('param3t3r');
+function repeatcontent(int $count = 1): void
+{
+    for ( $i =0; $i <= $count; $i++){
+        first("hi");
+    }
+}
+repeatcontent();
+repeatcontent(5);
