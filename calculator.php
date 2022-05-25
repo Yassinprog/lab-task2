f393de2c284d1058aedd552d9c55037a4f552c13
Yassinprog <yassinkarrat10gmail.com>
2022-05-25 15:03:08 +0200

    task8

--- calculator.php
+++ calculator.php
@@ -13,3 +13,32 @@ echo sum(10,2)."<br/>";
 echo diff(10,2)."<br/>";
 echo div(10,2)."<br/>";
 echo mult(10,2)."<br/>";
+
+function calculator(float $num1,string $opp,float $num2,):float{
+  if (!($opp == '+' or $opp == '-' or $opp == '/' or $opp == '*')){
+      return 0;
+  }  
+  switch ($opp){
+      case '+': return sum($num1,$num2);
+      case '-': return diff($num1,$num2);
+      case '/': return div($num1,$num2);
+      case '*': return mult($num1,$num2);
+  }
+}
+echo calculator(1,'+',2)."<br/>";
+echo calculator(1,'/',2)."<br/>";
+echo calculator(1,'-',2)."<br/>";
+echo calculator(1,'*',2)."<br/>";
+function calculator2(float $num1,string $opp,float $num2,):float{
+    if (!in_array($opp,['+','-','/','*'])){
+        return 0;
+    }  
+    $funct_name=['+' => 'sum',
+                    '-' => 'diff',
+                       '/' => 'div',
+                          '*' => 'mult',];
+         return $funct_name[$opp]($num1,$num2);
+        
+    }
+  
+  echo calculator(1,'+',3)."<br/>";
